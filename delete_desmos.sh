6b59d18e3ada27ac5077f7f5c69a1c64b1ab5e3d
Sergey <52304443+c29r3@users.noreply.github.com>
2020-10-07 23:38:03 +0300

    Update delete_desmos.sh

--- delete_desmos.sh
+++ delete_desmos.sh
@@ -2,6 +2,6 @@
 
 systemctl stop desmos.service
 systemctl disable desmos.service
-rm /etc/systemd/system/desmos.service $HOME/desmos/*.zip
+rm /etc/systemd/system/desmosd.service $HOME/desmos/*.zip
 
 rm -rf ~/desmos/desmos* $HOME/.desmosd
