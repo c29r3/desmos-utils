af0fae2d4f39eae09ffc4ee8be685edb48f9b956
Sergey <52304443+c29r3@users.noreply.github.com>
2020-10-08 15:29:37 +0300

    Update unjail.sh

--- unjail.sh
+++ unjail.sh
@@ -7,6 +7,7 @@ while true;
 do 
     OPERATOR=$($DESMOSCLI q staking delegations $($DESMOSCLI keys list | jq -r .[0].address) | jq -r .[].validator_address)
     STATUS=$($DESMOSCLI query staking validator $OPERATOR --trust-node -o json | jq -r .status)
+    echo "Status $STATUS"
     if [[ $STATUS != "2" ]]; then
         echo "UNJAIL"
         $DESMOSCLI tx slashing unjail --from $MONIKER --gas-adjustment="1.5" --gas="auto" --gas-prices="0.025udaric" --chain-id=morpheus-10000 -y
