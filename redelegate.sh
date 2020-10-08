8febda946b06f3cebfa9497b70683d411f8b3df9
Sergey <52304443+c29r3@users.noreply.github.com>
2020-10-08 16:19:06 +0300

    Create redelegate.sh

---
+++ redelegate.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+BIN_FILE="/root/go/bin/desmoscli"
+WALLET_NAME="desmos"
+CHAIN_ID="morpheus-10000"
+SELF_ADDR=$($BIN_FILE keys list | jq -r .[0].address)
+DENOM="udaric"
+OPERATOR=$($BIN_FILE q staking delegations --chain-id $CHAIN_ID $SELF_ADDR | jq -r .[].validator_address)
+
+echo -e "Current address: $SELF_ADDR\nCurrent operator address: $OPERATOR"
+
+while true;
+do
+    BALANCE=$($BIN_FILE query account $SELF_ADDR -o json | jq -r .value.coins[0].amount)
+    echo CURRENT BALANCE IS: $BALANCE
+    REWARD=$(( $BALANCE - 20000000 ))
+
+    if (( $BALANCE >  100000000 )); then
+        echo "Let's delegate $REWARD of REWARD tokens to $SELF_ADDR"
+        # delegate balance
+        $BIN_FILE tx staking delegate $OPERATOR "$REWARD"$DENOM --chain-id $CHAIN_ID --gas-adjustment 1.5 --gas auto --gas-prices "0.025"$DENOM --from $WALLET_NAME -y
+
+    else
+        echo "Reward is $REWARD"
+    fi
+    sleep 500
+done
+echo "DONE"
