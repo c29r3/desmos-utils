5229ea2879472fc969975935190d82cdb4307592
Sergey <52304443+c29r3@users.noreply.github.com>
2020-11-20 03:05:36 +0300

    Update redelegate.sh

--- redelegate.sh
+++ redelegate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 BIN_FILE="/root/go/bin/desmoscli"
+RPC_PORT="26657"
 WALLET_NAME="desmos"
 CHAIN_ID="morpheus-10000"
 SELF_ADDR=$($BIN_FILE keys list -o json | jq -r '.[] | select(.name=="desmos") | .address')
@@ -18,7 +19,7 @@ do
     if (( $BALANCE >  899999 )); then
         echo "Let's delegate $REWARD of REWARD tokens to $SELF_ADDR"
         # delegate balance
-        $BIN_FILE tx staking delegate $OPERATOR "$REWARD"$DENOM --chain-id $CHAIN_ID --gas-adjustment 1.5 --gas="200000" --gas-prices "0.025"$DENOM --from $SELF_ADDR -y
+        $BIN_FILE tx staking delegate $OPERATOR "$REWARD"$DENOM --chain-id $CHAIN_ID --node tcp://localhost:$RPC_PORT --gas-adjustment 1.5 --gas="200000" --fees 333333$DENOM --from $SELF_ADDR -y
 
     else
         echo "Reward is $REWARD"
