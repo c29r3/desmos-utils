5c78750f2a5a7c2325883bccbfc146d717476b78
Sergey <52304443+c29r3@users.noreply.github.com>
2020-11-20 03:03:07 +0300

    Update redelegate.sh

--- redelegate.sh
+++ redelegate.sh
@@ -3,9 +3,9 @@
 BIN_FILE="/root/go/bin/desmoscli"
 WALLET_NAME="desmos"
 CHAIN_ID="morpheus-10000"
-SELF_ADDR=$($BIN_FILE keys list | grep address | awk '{print $2}')
+SELF_ADDR=$($BIN_FILE keys list -o json | jq -r '.[] | select(.name=="desmos") | .address')
 DENOM="udaric"
-OPERATOR=$($BIN_FILE q staking delegations --chain-id $CHAIN_ID $SELF_ADDR | jq -r .[].validator_address)
+OPERATOR=$($BIN_FILE q staking delegations -o json --chain-id $CHAIN_ID $SELF_ADDR | jq -r .[].validator_address)
 
 echo -e "Current address: $SELF_ADDR\nCurrent operator address: $OPERATOR"
 
@@ -13,12 +13,12 @@ while true;
 do
     BALANCE=$($BIN_FILE query account $SELF_ADDR -o json | jq -r .value.coins[0].amount)
     echo CURRENT BALANCE IS: $BALANCE
-    REWARD=$(( $BALANCE - 200000 ))
+    REWARD=$(( $BALANCE - 333333 ))
 
     if (( $BALANCE >  899999 )); then
         echo "Let's delegate $REWARD of REWARD tokens to $SELF_ADDR"
         # delegate balance
-        $BIN_FILE tx staking delegate $OPERATOR "$REWARD"$DENOM --chain-id $CHAIN_ID --gas-adjustment 1.5 --gas="200000" --gas-prices "0.01"$DENOM --from $WALLET_NAME -y
+        $BIN_FILE tx staking delegate $OPERATOR "$REWARD"$DENOM --chain-id $CHAIN_ID --gas-adjustment 1.5 --gas="200000" --gas-prices "0.025"$DENOM --from $SELF_ADDR -y
 
     else
         echo "Reward is $REWARD"
