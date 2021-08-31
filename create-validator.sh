ebe9bf0e3bd9cf8589a34e215c1f9df498229ba3
Sergey <52304443+c29r3@users.noreply.github.com>
2021-08-31 20:33:44 +0300

    Update create-validator.sh

--- create-validator.sh
+++ create-validator.sh
@@ -1,13 +1,13 @@
-desmoscli tx staking create-validator \
-  --amount=3000000udaric \
-  --pubkey=$(desmosd tendermint show-validator) \
-  --moniker=$(cat /root/desmos/acc_name.txt) \
-  --chain-id=morpheus-10000 \
-  --commission-rate="0.10" \
-  --commission-max-rate="0.20" \
-  --commission-max-change-rate="0.01" \
-  --min-self-delegation="1" \
-  --gas="auto" \
-  --gas-adjustment="1.2" \
-  --gas-prices="0.025udaric" \
-  --from=desmos
+desmos tx staking create-validator \
+--amount=3000000udsm \
+--pubkey=$(desmos tendermint show-validator) \
+--moniker=<MONIKER> \
+--chain-id=desmos-mainnet \
+--commission-rate="0.10" \
+--commission-max-rate="0.20" \
+--commission-max-change-rate="0.01" \
+--min-self-delegation="1" \
+--gas="auto" \
+--gas-adjustment="1.2" \
+--gas-prices="0.025udsm" \
+--from=<WALLET_NAME>
